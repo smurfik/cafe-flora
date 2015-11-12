3dbca803e1337d76032a881f0aa8b102c2a9eba3
smurfik <smurfik@mlpinit.com>
2015-11-12 15:18:47 -0800

    add extra spaces to align '=' sign

--- app/controllers/menu_sections_controller.rb
+++ app/controllers/menu_sections_controller.rb
@@ -1,13 +1,13 @@
 class MenuSectionsController < ApplicationController
 
   def index
-    @menu_sections = MenuSection.all
-    @menu_items = MenuItem.all
+    @menu_sections            = MenuSection.all
+    @menu_items               = MenuItem.all
   end
 
   def show
-    @menu_section  = MenuSection.find(params[:id])
-    @menu_items    = MenuItem.all
+    @menu_section             = MenuSection.find(params[:id])
+    @menu_items               = MenuItem.all
   end
 
   def new
@@ -21,7 +21,7 @@ class MenuSectionsController < ApplicationController
     if @menu_section.save
       redirect_to "/menu_sections/#{@menu_section.id}"
     else
-      @menu_sections = MenuSection.all
+      @menu_sections          = MenuSection.all
       render :new
     end
   end
