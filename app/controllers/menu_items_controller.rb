858a6c3a9b9ddf93407de16480e4fcd51192a02e
Daniel Adler <weston.adler@gmail.com>
2015-11-12 11:53:16 -0800

    removed extraneous comments

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -9,18 +9,14 @@ class MenuItemsController < ApplicationController
   end
 
   def add
-    # raise
     @menu_item                 = MenuItem.new
     @menu_item.name            = params[:menu_item_name]
     @menu_item.price           = params[:menu_item_price]
     @menu_item.description     = params[:menu_item_description]
     @menu_item.note            = params[:menu_item_note]
-    @menu_item.menu_section_id = params[:id] # *** new
-
-    # raise
-    # @menu_item.save
+    @menu_item.menu_section_id = params[:id]
     if @menu_item.save
-      redirect_to "/menu_sections/#{@menu_item.menu_section_id}" # *** updated
+      redirect_to "/menu_sections/#{@menu_item.menu_section_id}"
     else
       # not sure if this part is right for new menu items (ok for sections)
       @menu_sections = MenuSection.all
