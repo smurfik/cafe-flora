b0f3949274d235f1a0ec669337c85ce62c72416d
smurfik <smurfik@mlpinit.com>
2015-11-18 11:24:13 -0800

    delete jQuery and unused index view

--- app/assets/javascripts/edit.js
+++ app/assets/javascripts/edit.js
@@ -1,24 +1,3 @@
 $(document).ready(function() {
 
-  $('#full').click(function(){
-      var name = $(this).text();
-      $(this).html('');
-      $('<input></input>')
-          .attr({
-              'type': 'text',
-              'name': 'fname',
-              'id': 'txt_full',
-              'size': '30',
-              'value': name
-          })
-          .appendTo('#full');
-      $('#txt_full').focus();
-  });
-
-  $(document).on('blur','#txt_full', function(){
-      var name = $(this).val();
-      //alert('Make an AJAX call and pass this parameter >> name=' + name);
-      $('#full').text(name);
-  });
-
 });
