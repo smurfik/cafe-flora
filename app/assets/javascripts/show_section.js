047a6a6a63e8e5e6a0366f52ae4bcd1fb528698c
Daniel Adler <weston.adler@gmail.com>
2015-11-20 15:01:49 -0800

    update JS and layout for /menu_section/show to include forms for each section on hover

---
+++ app/assets/javascripts/show_section.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+  $(".item-details").hide();
+  $(".item-forms").hide();
+  var top_sec = $(".section-name").first();
+  var top_sec_id = top_sec.attr("class").split(' ')[1];
+  $(".item-details"+"."+ top_sec_id).show();
+  top_sec.css('background-color', 'rgba(0, 0, 0, 0.10)');
+  $(".item-forms"+"."+ top_sec_id).show();
+
+  $(".section-name").hover(function () {
+    $(".section-name").css('background-color', 'white');
+    $(this).css('background-color', 'rgba(0, 0, 0, 0.10)')
+    $(".item-forms").hide();
+    $(".item-details").hide();
+    var sec_id = ($(this).attr("class").split(' ')[1]);
+    $(".item-details"+"."+ sec_id).toggle();
+    $(".item-forms"+"."+ sec_id).toggle();
+  });
+
+});
