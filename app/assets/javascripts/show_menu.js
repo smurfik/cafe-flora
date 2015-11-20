d94bbb5c3931217d0579cfde9ad42a589f88a876
Daniel Adler <weston.adler@gmail.com>
2015-11-20 13:24:12 -0800

    refactor JS and CSS for /menu/show to display section details on section name hover

--- app/assets/javascripts/show_menu.js
+++ app/assets/javascripts/show_menu.js
@@ -35,16 +35,32 @@
 
 $(document).ready(function() {
 
+  // set up the view on load
   $(".item-details").hide();
-  var top_sec = $(".section-name").first().attr("class").split(' ')[1];
-  $(".item-details"+"."+ top_sec).show();
+  var top_sec = $(".section-name").first();
+  var top_sec_id = top_sec.attr("class").split(' ')[1];
+  $(".item-details"+"."+ top_sec_id).show();
+  top_sec.css('background-color', 'rgba(0, 0, 0, 0.10)');
 
-  $(".section-name").click(function () {
-  $(".item-details").hide();
-  var sec = ($(this).attr("class").split(' ')[1]);
-  $(".item-details"+"."+ sec).toggle();
-  return false;
+  $(".section-name").hover(function () {
+    $(".section-name").css('background-color', 'white');
+    $(this).css('background-color', 'rgba(0, 0, 0, 0.10)')
+    $(".item-details").hide();
+    var sec = ($(this).attr("class").split(' ')[1]);
+    $(".item-details"+"."+ sec).toggle();
   });
+
+  // working code in case new stuff doesn't work out!
+  // $(".item-details").hide();
+  // var top_sec = $(".section-name").first().attr("class").split(' ')[1];
+  // $(".item-details"+"."+ top_sec).show();
+  //
+  // $(".section-name").click(function () {
+  //   $(".item-details").hide();
+  //   var sec = ($(this).attr("class").split(' ')[1]);
+  //   $(".item-details"+"."+ sec).toggle();
+  //   return false;
+  // });
 
 });
 
