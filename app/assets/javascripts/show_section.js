07460863b42e1fed8eb31ed0c578e46df779ec04
Daniel Adler <weston.adler@gmail.com>
2015-11-22 20:17:36 -0800

    additional aesthetic updates

--- app/assets/javascripts/show_section.js
+++ app/assets/javascripts/show_section.js
@@ -5,12 +5,12 @@ $(document).ready(function() {
   var top_sec = $(".section-name").first();
   var top_sec_id = top_sec.attr("class").split(' ')[1];
   $(".item-details"+"."+ top_sec_id).show();
-  top_sec.css('background-color', 'rgba(0, 0, 0, 0.10)');
+  top_sec.css('background-color', 'rgba(0, 0, 0, 0.06)');
   $(".item-forms"+"."+ top_sec_id).show();
 
   $(".section-name").hover(function () {
     $(".section-name").css('background-color', 'transparent');
-    $(this).css('background-color', 'rgba(0, 0, 0, 0.10)');
+    $(this).css('background-color', 'rgba(0, 0, 0, 0.06)');
     $(".item-forms").hide();
     $(".item-details").hide();
     var sec_id = ($(this).attr("class").split(' ')[1]);
